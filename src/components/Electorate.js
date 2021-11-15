1e3e9d75016890bddf8483abdc877b18eca2d224
dmarmion <dmarmion08@gmail.com>
2021-11-15 18:34:32 +1100

    Blank out cells without an electorate

--- src/components/Electorate.js
+++ src/components/Electorate.js
@@ -1,11 +1,26 @@
 import React from "react";
 
 class Electorate extends React.Component {
+  /*
+   * Get a string containing the CSS classes (aside from 'electorate') that this
+   * component should have. This is primarily used to determine the colour of the
+   * electorate.   
+   */
+  getElectorateClasses() {
+    let classes = "";
+    const delimiter = " ";
+
+    // The square should be blank if it does not contain an electorate
+    if (this.props.name === "none") {
+      classes += "electorate-none" + delimiter;
+    }
+
+    return classes;
+  }
+
   render() {
     return (
-      <div className="electorate">
-        <p>{this.props.name === "none" ? "." : this.props.name}</p>
-      </div>
+      <div className={"electorate " + this.getElectorateClasses()}></div>
     );
   } 
 }
