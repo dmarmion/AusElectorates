d3790e6c38914d042638a8eb165afd3099bccdb4
dmarmion <dmarmion08@gmail.com>
2022-04-02 22:43:20 +1100

    Covert Electorate to function component

--- src/components/Electorate.js
+++ src/components/Electorate.js
@@ -1,27 +1,21 @@
 import React from "react";
 import { getPartyClassMap } from "../utils";
 
-class Electorate extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleSelection = this.handleSelection.bind(this);
-    this.handleDeselection = this.handleDeselection.bind(this);
-  }
-
+const Electorate = ({ name, party, onSelectionChange }) => {
   /*
    * Get a string containing the CSS classes (aside from 'electorate') that this
    * component should have. This is primarily used to determine the colour of the
    * electorate.   
    */
-  getElectorateClasses() {
+  const getElectorateClasses = () => {
     let classes = "";
     const delimiter = " ";
 
     // The square should be blank if it does not contain an electorate
-    if (this.props.name === "none") {
+    if (name === "none") {
       classes += "electorate-none" + delimiter;
     } else {
-      let partyClass = getPartyClassMap().get(this.props.party);
+      let partyClass = getPartyClassMap().get(party);
 
       if (partyClass === undefined) {
         partyClass = "";
@@ -33,32 +27,14 @@ class Electorate extends React.Component {
     return classes.trimEnd();
   }
 
-  /*
-   * Update the currently selected electorate in the map component when the
-   * mouse enters this electorate
-   */
-  handleSelection() {
-    this.props.onSelectionChange(this.props.name);
-  }
-
-  /* 
-   * Reset the currently selected electorate in the map component to no
-   * electorate when the mouse leaves this electorate
-   */
-  handleDeselection() {
-    this.props.onSelectionChange("none");
-  }
-
-  render() {
-    return (
-      <div
-        className={"electorate " + this.getElectorateClasses()}
-        onMouseEnter={this.handleSelection}
-        onMouseLeave={this.handleDeselection}
-      >
-      </div>
-    );
-  } 
+  return (
+    <div
+      className={"electorate " + getElectorateClasses()}
+      onMouseEnter={() => onSelectionChange(name)}
+      onMouseLeave={() => onSelectionChange("none")}
+    >
+    </div>
+  );
 }
 
 export default Electorate;
