824d6824f80d78463b7d9f2f8ccc98878e2d5ac8
dmarmion <dmarmion08@gmail.com>
2022-04-09 21:19:07 +1000

    Convert ElectorateMap to function component

--- src/components/ElectorateMap.js
+++ src/components/ElectorateMap.js
@@ -1,31 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 import DataSelector from "./DataSelector";
 import Electorate from "./Electorate";
 
-class ElectorateMap extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      // By default, use the current state of parliament
-      locations: require("../data/locations2019.json"),
-      parties: require("../data/partiespre2022.json"),
-      currentElectorate: "none"
-    };
-
-    this.handleCartogramDataChange = this.handleCartogramDataChange.bind(this);
-    this.handleElectorateChange = this.handleElectorateChange.bind(this);
-  }
+function ElectorateMap() {
+  // By default, use the current state of federal parliament
+  const [locations, setLocations] = useState(require("../data/locations2019.json"));
+  const [parties, setParties] = useState(require("../data/partiespre2022.json"));
+  const [currentElectorate, setCurrentElectorate] = useState("none");
 
   // Render the grid of electorates
-  renderElectorates() {
-    const tableRows = this.state.locations.rows;
-    const tableCols = this.state.locations.columns;
-    
+  const renderElectorates = () => {    
     // Helper function to generate the rows of the table
     const renderGridRows = () => {
       let rows = [];
-      for (let row = 0; row < tableRows; row++) {
+      for (let row = 0; row < locations.rows; row++) {
         rows.push(
           <tr key={row}>
             {renderGridColumns(row)}
@@ -38,16 +27,16 @@ class ElectorateMap extends React.Component {
     // Helper function to generate the columns in each row
     const renderGridColumns = (row) => {
       let cols = [];
-      for (let col = 0; col < tableCols; col++) {
-        let seatName = this.state.locations[`${col},${row}`];
-        let party = (seatName === "none" ? "" : this.state.parties[seatName]);
+      for (let col = 0; col < locations.columns; col++) {
+        let seatName = locations[`${col},${row}`];
+        let party = (seatName === "none" ? "" : parties[seatName]);
 
         cols.push(
           <td key={`${row}-${col}`}>
             <Electorate
               name={seatName}
               party={party}
-              onSelectionChange={this.handleElectorateChange}
+              onSelectionChange={handleElectorateChange}
             />
           </td>
         )
@@ -55,47 +44,43 @@ class ElectorateMap extends React.Component {
       return cols;
     };
 
-    let tableElements = (
+    return (
       <table className="electorate-map">
         <tbody>
           {renderGridRows()}
         </tbody>
       </table>
-    )
-
-    return tableElements;
+    );
   }
 
   // Update the data used to construct the cartogram
-  handleCartogramDataChange(locations, parties) {
-    this.setState({locations: locations, parties: parties});
+  const handleCartogramDataChange = (locations, parties) => {
+    setLocations(locations);
+    setParties(parties);
   }
 
   // Update the currently selected electorate
-  handleElectorateChange(electorate) {
-    this.setState({currentElectorate: electorate});
+  const handleElectorateChange = (electorate) => {
+    setCurrentElectorate(electorate);
   }
 
-  render() {
-    return (
-      <div className="row">
-        <div className="col-md-9">
-          {this.renderElectorates()}
-          <h4>
-            Currently selected: {
-              this.state.currentElectorate === "none"
-                ? ""
-                : `${this.state.currentElectorate} ` +
-                  `(${this.state.parties[this.state.currentElectorate]})`
-            }
-          </h4>
-        </div>
-        <div className="col-md-3">
-          <DataSelector updateMapData={this.handleCartogramDataChange}/>
-        </div>
+  return (
+    <div className="row">
+      <div className="col-md-9">
+        {renderElectorates()}
+        <h4>
+          Currently selected: {
+            currentElectorate === "none"
+              ? ""
+              : `${currentElectorate} (${parties[currentElectorate]})`
+          }
+        </h4>
       </div>
-    );
-  }
+      <div className="col-md-3">
+        <DataSelector updateMapData={handleCartogramDataChange}/>
+      </div>
+    </div>
+  );
 }
 
 export default ElectorateMap;
