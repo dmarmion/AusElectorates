4f38d1b146fdafb10bbb5d75428a52fbef54bc2f
dmarmion <dmarmion08@gmail.com>
2022-11-01 21:48:45 +1100

    Add rounded corners to cartogram

--- src/components/Electorate.js
+++ src/components/Electorate.js
@@ -2,14 +2,14 @@ import React from "react";
 
 import { NO_PARTY_COLOUR, PARTY_COLOURS } from "../constants";
 
-const Electorate = ({ name, party, onSelectionChange }) => {
+const Electorate = ({ name, party, onSelectionChange, cornerStyles }) => {
   const partyColour = party ? PARTY_COLOURS.get(party) : NO_PARTY_COLOUR;
   return (
     <div
       className="electorate"
       onMouseEnter={() => onSelectionChange(name)}
       onMouseLeave={() => onSelectionChange(null)}
-      style={{ backgroundColor: partyColour }}
+      style={{ backgroundColor: partyColour, ...cornerStyles }}
     ></div>
   );
 };
--- src/components/ElectorateMap.js
+++ src/components/ElectorateMap.js
@@ -9,6 +9,34 @@ const ElectorateMap = () => {
   const [parties, setParties] = useState(require("../data/federal/parties2022.json"));
   const [currentElectorate, setCurrentElectorate] = useState(null);
 
+  const handleCartogramDataChange = (locations, parties) => {
+    setLocations(locations);
+    setParties(parties);
+  };
+
+  const handleElectorateChange = (electorate) => {
+    setCurrentElectorate(electorate);
+  };
+
+  const locationKey = (col, row) => `${col},${row}`;
+
+  const getCornerStyles = (col, row) => {
+    const RADIUS = "30%";
+    let styles = {};
+
+    const seatExistsToLeft = !!locations[locationKey(col - 1, row)];
+    const seatExistsAbove = !!locations[locationKey(col, row - 1)];
+    const seatExistsToRight = !!locations[locationKey(col + 1, row)];
+    const seatExistsBelow = !!locations[locationKey(col, row + 1)];
+
+    if (!seatExistsToLeft && !seatExistsAbove) styles["borderTopLeftRadius"] = RADIUS;
+    if (!seatExistsAbove && !seatExistsToRight) styles["borderTopRightRadius"] = RADIUS;
+    if (!seatExistsToRight && !seatExistsBelow) styles["borderBottomRightRadius"] = RADIUS;
+    if (!seatExistsBelow && !seatExistsToLeft) styles["borderBottomLeftRadius"] = RADIUS;
+
+    return styles;
+  };
+
   // Render the grid of electorates
   const renderElectorates = () => {
     // Helper function to generate the rows of the table
@@ -24,12 +52,17 @@ const ElectorateMap = () => {
     const renderGridColumns = (row) => {
       let cols = [];
       for (let col = 0; col < locations.columns; col++) {
-        let seatName = locations[`${col},${row}`];
+        let seatName = locations[locationKey(col, row)];
         let party = seatName ? parties[seatName] : "";
 
         cols.push(
           <td key={`${row}-${col}`}>
-            <Electorate name={seatName} party={party} onSelectionChange={handleElectorateChange} />
+            <Electorate
+              name={seatName}
+              party={party}
+              onSelectionChange={handleElectorateChange}
+              cornerStyles={getCornerStyles(col, row)}
+            />
           </td>
         );
       }
@@ -41,17 +74,6 @@ const ElectorateMap = () => {
         <tbody>{renderGridRows()}</tbody>
       </table>
     );
-  };
-
-  // Update the data used to construct the cartogram
-  const handleCartogramDataChange = (locations, parties) => {
-    setLocations(locations);
-    setParties(parties);
-  };
-
-  // Update the currently selected electorate
-  const handleElectorateChange = (electorate) => {
-    setCurrentElectorate(electorate);
   };
 
   return (
